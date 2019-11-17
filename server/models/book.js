8308a1e506c0a49e5e8381dfd7be96b69e0ce5af
Nick Dyson <qwoxa1@gmail.com>
2019-11-17 06:22:13 -0500

    graphql backend

---
+++ server/models/book.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  name: String,
+  genre: String,
+  authorId: {
+    type: Schema.Types.ObjectId,
+    ref: 'author'
+  }
+});
+
+module.exports = mongoose.model('book', bookSchema);
--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+// Connect to db
+(async () => {
+  try {
+    const URI = 'mongodb://localhost:27017/graphql';
+    const options = { useUnifiedTopology: true, useNewUrlParser: true };
+    await mongoose.connect(URI, options);
+    console.log('DB connected');
+  } catch(error) {
+    console.log(`DB connection error`, error);
+  }
+})();
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-
+  schema,
+  graphiql: true
 }));
 
 app.listen(process.env.PORT, () => {
